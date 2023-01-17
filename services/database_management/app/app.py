d9475606b330dea77d967941cb39381a7a4fcd1d
KrishnaKothandaraman <kkrishna0701@gmail.com>
2023-01-17 15:23:01 +0800

    env removed

--- services/database_management/app/app.py
+++ services/database_management/app/app.py
@@ -1,13 +1,3 @@
-import os
-
-os.environ["remote-check-api-key"] = "hu685pkl-0lr1-7jx5-r3be-yzxjaqnrzfm9"
-os.environ["appsheet-accesskey"] = "V2-mwTj5-nvH7J-6sv08-6yfrG-OSs2H-jQEOe-sWbY9-mZ5ha"
-os.environ["rf-token"] = "Plain e01e77cd-899f-4964-a44f-ec603ab62d17"
-os.environ["bm-token"] = "YmFjazJsaWZlcHJvZHVjdHNAb3V0bG9vay5jb206ODMyNzhydWV3ZmI3MzpmbmopKE52OCY4"
-os.environ["swd-shopkey"] = "9q7qDKnCX+XHtdAka96OTm3SEfyW/0gw/5HYQJClmtBet4grGl1/W5xuGxmuSGM2B/R5OADgmn+z" \
-                            "+4GnkBtRapMqxRaOcpfqpMeLPDO4qGAlNmVCzXyFNbYHPA4ORmRHBo5pnRKmVtamFPjJh3BLdaSsYCD6IxckVJb8f6N10Kc= "
-os.environ["swd-shopid"] = "11025"
-
 from flask_cors import CORS
 from flask import Flask
 import services.database_management.app.controller.addorders_controller as addorders_controller
