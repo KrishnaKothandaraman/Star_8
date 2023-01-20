de8877b7c9aa4aee963dd13d06d6fe8d09a1f401
KrishnaKothandaraman <kkrishna0701@gmail.com>
2023-01-20 16:29:04 +0800

    Refactor controller names

--- services/database_management/app/app.py
+++ services/database_management/app/app.py
@@ -1,13 +1,14 @@
 from flask_cors import CORS
 from flask import Flask
-import services.database_management.app.controller.addorders_controller as addorders_controller
+import services.database_management.app.controller.orders_controller as orders_controller
 import services.database_management.app.controller.orders_database_controller as db_controller
 
 app = Flask(__name__)
 CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
-app.add_url_rule('/swd-add-order', methods=["POST"], view_func=addorders_controller.swdAddOrder)
+app.add_url_rule('/swd-add-order', methods=["POST"], view_func=orders_controller.swdAddOrder)
 app.add_url_rule('/update-googlesheet', methods=["POST"], view_func=db_controller.updateGoogleSheet)
+app.add_url_rule('/update-tracking-info', methods=["POST"], view_func=orders_controller.updateTrackingInfo)
 
 
 def create_api_app():
