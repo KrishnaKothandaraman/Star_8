3ebe873e762c3629780b1d030af38cac5731c62d
KrishnaKothandaraman <kkrishna0701@gmail.com>
2023-07-14 09:16:36 +0400

    Remove app sheet feature

--- services/database_management/app/controller/utils/general_utils.py
+++ services/database_management/app/controller/utils/general_utils.py
@@ -1,15 +1,20 @@
+import asyncio
 import json
-from typing import List
+import time
+from typing import List, Literal
 
+import aiohttp
 import requests
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 APP_SHEET_ACCESS_KEY = os.environ["APPSHEETACCESSKEY"]
+URL = str
 
 
 def updateAppSheetWithRows(rows: List):
+    return
     resp = requests.post(
         url='https://api.appsheet.com/api/v2/apps/6aec3910-fe2b-4d41-840e-aee105698fe3/tables/Order_Notice/Add',
         headers={'Content-Type': 'application/json',
@@ -32,7 +37,6 @@ def updateAppSheetWithRows(rows: List):
     else:
         print(f"Update sheet failed! Code: {resp.status_code} message: {resp.reason}")
 
-
 # def getShipperName(price: float, chosenShipperName: str, country_code: str, remoteCheckCode):
 #     # last condition in the IF is to filter out any shippers such as UPS Express or DHL Express
 #     if price < 800 and remoteCheckCode == 204 and len(chosenShipperName.split(" ")) == 1 and \
@@ -41,6 +45,7 @@ def updateAppSheetWithRows(rows: List):
 #     else:
 #         return "dhlexpress"
 
+
 def getShipperName(price: float, chosenShipperName: str, country_code: str):
     # last condition in the IF is to filter out any shippers such as UPS Express or DHL Express
     if price < 800 and len(chosenShipperName.split(" ")) == 1 and \
@@ -48,3 +53,18 @@ def getShipperName(price: float, chosenShipperName: str, country_code: str):
         return "ups"
     else:
         return "dhlexpress"
+
+
+def getCurrentPriceOfSku(sku, skuMap, vendor: Literal["BM", "RF"]):
+    return skuMap[sku][vendor][sku[-2:]]
+
+
+async def get_results_from_urls(urls: List[URL]) -> List:
+    results = []
+    async with aiohttp.ClientSession() as session:
+        tasks = [session.get(url) for url in urls]
+        responses = await asyncio.gather(*tasks)
+        for response in responses:
+            results.append(await response.json())
+
+    return results
